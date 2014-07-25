03c2d1aad2f6ff4d3646f793be7358858743ef23
Max Gaukler <development@maxgaukler.de>
2014-07-25 14:44:00 +0200

    Config ausgelagert, Doku

---
+++ config.example.h
@@ -0,0 +1,12 @@
+// Alle Dokumentation zu den Einstellungen siehe defines.h
+
+#define HW_0_4
+#define DISP_FRANKEN
+#define POWER_LED PC0
+
+// #define DISPLAY_DIMMEN
+#define DISPLAY_SCROLL
+#define WORDCLOCK_MIRROR
+// #define WORDCLOCK_MIRROR_ZEILEN
+#define nightSyncTime 6
+#define MINUTEN_PHIL
