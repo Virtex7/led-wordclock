d060c5fa8c2c2bfa58422125a057ea08a03a0b89
Max Gaukler <development@maxgaukler.de>
2014-08-13 13:37:05 +0200

    merge Virtex7/master

--- config.example.h
+++ config.example.h
@@ -1,3 +1,4 @@
+// Beispielkonfiguration
 // Alle Dokumentation zu den Einstellungen siehe defines.h
 
 #define HW_0_4
@@ -9,3 +10,13 @@
 // #define WORDCLOCK_MIRROR_ZEILEN
 #define nightSyncTime 6
 #define MINUTEN_PHIL
+
+// #define DEBUG_TIMER
+#define DEBUG_STATUS
+#define DEBUG_ERFASSUNG
+// #define DEBUG_DATENWERT
+#define DEBUG_ZEIT
+#define DEBUG_ZEITAUSGABE
+#define DEBUG_RTC
+#define DEBUG_RTC_READ
+#define DEBUG_DISPLAY
--- defines.h
+++ defines.h
@@ -5,7 +5,7 @@
 #define RTC_SET_MIN 59
 #define RTC_SET_STD 2
 
-//Algemeiner Defines Block
+//Allgemeiner Defines-Block
 #include "../AtmelLib/global.h"
 #include "../AtmelLib/io/io.h"
 #include <stdio.h>
@@ -20,14 +20,17 @@
  *
  * #define HW_0_4
  * Für die aktuelle Hardwareversion. Zur Zeit werden keine anderen unterstützt.
+ * Wird auch in der Debug-Anzeige angezeigt.
  * 
  * # define DISP_xxxx
  * um das Display-Layout auszuwählen. Es gibt:
+ * Dieses Layout muss zum aufgebauten Display passen!
  *
  * DISP_NORM:
  * 
  * ESKISTAFÜNF
  * ZEHNDAUVORG
+ * NACHVIERTEL
  * HALBVORNACH
  * EINSKURZWEI
  * DREIAUJVIER
@@ -37,14 +40,29 @@
  * ZWÖLFDTFELF
  * WASDFUNKUHR
  * 
+ * 
  * DISP_FRANKEN:
  * 
- * TODO Doku
+ * ESKISTLFÜNF
+ * DAUERTXKURZ
+ * ZENHZWANZIG
+ * DREIVIERTEL
+ * NACHMVORJGZ
+ * HALBFUNKUHR
+ * DRZWÖLFÜNFX
+ * ZEHNEUNDREI
+ * ZWEINSIEBEN
+ * ELFVIERACHT
+ * SECHSIUHRYE
+ * 
  * 
  * DISP_HORM:
  * 
- * TODO Doku
+ * Spezialversion für die Gemeinde "Hormersdorf" und deren sehr spezielle
+ * Art, die Uhrzeit zu sagen. Mehr Info unter:
+ * http://www.hormersdorf-erzgebirge.de/mundart.html
  *
+ * xxx
  * 
  * 
  * Weitere Schalter nach Bedarf:
@@ -79,11 +97,53 @@
  * Nur während dieser Zeit kann DCF-Empfang stattfinden, da das LED-PWM den Empfang stört. Also ist 0 nicht erlaubt!
  * 
  * 
+ * DEBUG-Einstellungen (auch in der config.h zu setzen)
+ * 
+ * LED_ROT = Anzeige
+ * LED_GELB = Sync in der Nacht nicht erfolgreich
+ * LED_GRÜN = DCF-SIGNAL
+ *
+ * UART-Einstellung: 57600 BAUD, 1 Stoppbits, 8 Bit Frame, no Parity
+ *
+ *
+ * DEBUG_TIMER:
+ * Ausgabe des erfassten Timerwertes in ms für den High-Pegel des DCF-Signals (roh) aus.
+ *
+ * DEBUG_ERFASSUNG:
+ * gibt die erfassten DCF-Bits aus (60Bit Wort, LSB first)
+ *
+ * DEBUG_DATENWERT:
+ * Ausgabe des Teils des erfassten Wertes, in dem die Uhrzeit steht.
+ * (binär, Reihenfolge wie DCF)
+ *
+ * DEBUG_ZEIT
+ * Ausgabe der berechneten Uhrzeit, wenn die DCF-Uhr synct.
+ * 
+ * DEBUG_ZEITAUSGABE
+ * Gibt die jetzt-Zeit aus, wenn die Uhr auf RTC läuft.
+ * ACHTUNG: Ausgabe jede Sekunde!
+ *
+ * DEBUG_RTC
+ * gibt den Zeitwert der RTC und den aktuellen DCF-Wert in BCD-Code aus
+ * beide Werte stehen zum besseren Vergleich untereinander.
+ * 
+ * DEBUG_RTC_RESET
+ * Setzt die RTC auf jeden Fall zurück, auch wenn schon eine valide
+ * Uhrzeit drinsteht. Für erzwungenen DCF-Empfang verwenden.
+ *
+ * RTC_3UHR_TEST
+ * Setzt die RTC am Anfang der Main auf kurz vor drei Uhr
+ * 
+ * DEBUG_RTC_READ
+ * UART-Ausgabe beim dauerhaften Auslesen der RTC (viel Output!)
+ * genaue Anzeige der aktuellen Uhrzeit ist gewährleistet
+ *
+ * DEBUG_DISPLAY
+ * Alle LEDs des Displays sollten jetzt leuchten, danach Uhr-Funktionen
+ * Wichtig, um Lötfehler ausschließen zu können
+ *
  */
 
-
-// DEBUG-Verhalten
-#include "./debug.h"
 
 // Globale Variablen:
 volatile uint8_t status = 0;
@@ -117,7 +177,7 @@ void clearTemp(void) {
 	}
 }
 
-// Definiere Hardwareversion und Displaylayout
+// Definiere Hardwareversion und Displaylayout nach config.h
 #ifdef HW_0_4
 #include "./HW_0_4.h"
 #define HW_VERSION "0_4"
